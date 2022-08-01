2ce34e70036ed466957ca8cb4bdca27c42e7956c
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-07-31 23:45:58 -0400

    added integration test to '/api/getallgateways', '/api/adddevice', '/api/delgateway'. Integration Test completed

--- test/integ-test.js
+++ test/integ-test.js
@@ -34,6 +34,34 @@ const gatewayArr = [
   },
 ];
 
+const deviceArr = [
+  {
+    uid: 1549154416,
+    vendor: "Madaih",
+    status: "online",
+  },
+  {
+    uid: 6069351410,
+    vendor: "Egbert",
+    status: "online",
+  },
+  {
+    uid: 8835714188,
+    vendor: "Fulmer",
+    status: "offline",
+  },
+  {
+    uid: 9239843776,
+    vendor: "Bibi",
+    status: "online",
+  },
+  {
+    uid: 7839712447,
+    vendor: "Scammon",
+    status: "online",
+  },
+];
+
 describe("Integration Test", () => {
   describe("Preparing Integration Test", () => {
     gatewayArr.forEach((gateway) => {
@@ -55,8 +83,8 @@ describe("Integration Test", () => {
     });
   });
 
-  //Adding 10 gateways to db
-  describe("Testing '/api/addgateway' entry point", () => {
+  //Creating 10 gateways in db
+  describe("Testing '/api/addgateway' entry point(Create)", () => {
     gatewayArr.forEach((gateway) => {
       it(`Adding gateway "${gateway.gatewayName}" to db`, function (done) {
         this.timeout(5000);
@@ -64,7 +92,8 @@ describe("Integration Test", () => {
           .request(server)
           .post("/api/addgateway")
           .send(gateway)
-          .end((_, res) => {
+          .end((err, res) => {
+            expect(err).to.be.null;
             if (res.status === 409) {
               expect(res.text).to.equal("Error: Gateway already exist");
             } else if (res.status === 400) {
@@ -83,6 +112,78 @@ describe("Integration Test", () => {
                 gateway.gatewayName
               );
               expect(res.body).to.have.property("ipv4", gateway.ipv4);
+            }
+            done();
+          });
+      });
+    });
+  });
+
+  //Retrieving all gateways from db
+  describe("Testing '/api/getallgateways' entry point(Retriev)", () => {
+    it("Retrieving all gateways from db", function (done) {
+      this.timeout(5000);
+      chai
+        .request(server)
+        .get("/api/getallgateways")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          res.body.forEach((gateway) => {
+            expect(gateway)
+              .to.have.property("gatewaySerial")
+              .that.is.a("string");
+            expect(gateway).to.have.property("gatewayName").that.is.a("string");
+            expect(gateway)
+              .to.have.property("associatedDevices")
+              .that.is.a("number");
+          });
+          done();
+        });
+    });
+  });
+
+  //Updating gateways by adding devices to each one
+  describe("Testing '/api/adddevice' entry point(Update)", () => {
+    deviceArr.forEach((device) => {
+      it(`Adding device '${device.uid}' to gateway '${gatewayArr[0].gatewayName}'`, function (done) {
+        this.timeout(5000);
+        chai
+          .request(server)
+          .put("/api/adddevice")
+          .send({
+            gatewaySerial: gatewayArr[0].gatewaySerial,
+            device,
+          })
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            const [returnedDevice] = res.body.devices.filter(
+              (dev) => dev.uid === device.uid
+            );
+            expect(returnedDevice).to.have.property("uid", device.uid);
+            expect(returnedDevice).to.have.property("vendor", device.vendor);
+            expect(returnedDevice).to.have.property("status", device.status);
+            done();
+          });
+      });
+    });
+  });
+
+  //Deleting all gateways
+  describe("Testing '/api/delgateway' entry point(Delete)", () => {
+    gatewayArr.forEach((gateway) => {
+      it(`Deleting gateway "${gateway.gatewayName}" from db`, function (done) {
+        this.timeout(5000);
+        chai
+          .request(server)
+          .delete("/api/delgateway")
+          .send({ gatewaySerial: gateway.gatewaySerial })
+          .end((_, res) => {
+            if (res.status === 404) {
+              expect(res.text).to.equal("Error: Gateway not found");
+            } else {
+              expect(res).to.have.status(200);
             }
             done();
           });
